cadcb398b85668c2e7acf553c87b695dc4ea46ea
timdoozy <thinds96@ufl.edu>
2019-04-16 16:44:14 -0400

    reflecting viewcard
    Implements what the viewcard component did.

--- src/app/navigator/navigator.component.ts
+++ src/app/navigator/navigator.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CardService } from '../services/card.service';
+import { CardInterface } from '../models/Card'
 
 @Component({
   selector: 'app-navigator',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigatorComponent implements OnInit {
 
-  constructor() { }
+  private cards: CardInterface[] = [];
+
+
+
+
+  constructor(private cardServ: CardService) { }
 
   ngOnInit() {
+    this.loadCards();
   }
+
+  public loadCards() {
+    //get all cards from server and update the cards property
+    this.cardServ.getAllCards().subscribe(
+      response => this.cards = response,)
+  }
+
+    /*deleteCard. The deleted card is being filtered out using the .filter method
+    public deleteCard(card: CardInterface) {
+      this.cardServ.deleteCard(card._id).subscribe(
+        response =>    this.cards = this.cards.filter(cards => cards !== card),)
+      }
+      */
 
 }
