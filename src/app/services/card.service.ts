f7afad65da1fd0282bf645719cc35075573401c2
timdoozy <thinds96@ufl.edu>
2019-04-19 21:35:50 -0400

    updated card service with error handling

--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -1,29 +1,41 @@
+/*card.service.ts*/
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import {Observable} from 'rxjs/Observable';
-import { CardInterface } from '../models/Card'
-
 import 'rxjs/add/operator/map';
-
-
+import { Observable, of, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError, tap, map } from 'rxjs/operators';
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+const apiUrl = "/api";
 @Injectable()
 export class CardService {
 
-    constructor(private http: Http) { }
-
-    //get all cards from api
-    getAllCards() {
-        return this.http.get('/api/card')
-            .map(res => res.json());
+  constructor(private http: HttpClient) { }
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
     }
-    /*
-    public getAllCards():Observable<CardInterface[]> {
-
-        let URI = `${this.serverApi}/card/`;
-        return this.http.get(URI)
-            .map(res => res.json())
-            .map(res => <CardInterface[]>res.cards);
-    }
-    */
-
+    // return an observable with a user-facing error message
+    return throwError('Something bad happened; please try again later.');
+  };
+  private extractData(res: Response) {
+    let body = res;
+    return body || { };
+  }
+  getAllCards(): Observable<any> {
+    return this.http.get(apiUrl, httpOptions).pipe(
+      map(this.extractData),
+      catchError(this.handleError));
+  }
+  
+  
 }
+
