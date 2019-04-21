81d8803a43d7944f90a80935d147ab31ddb6d65b
timdoozy <thinds96@ufl.edu>
2019-04-21 03:01:01 -0400

    updated the card view. It should be good for now, but we will need an "add to order" button next

--- src/app/home/components/side-filter/side-filter.component.spec.ts
+++ src/app/home/components/side-filter/side-filter.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import { SideFilterComponent } from './side-filter.component';
 
-describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+describe('SideFilterComponent', () => {
+  let component: SideFilterComponent;
+  let fixture: ComponentFixture<SideFilterComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [  ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
+    fixture = TestBed.createComponent(SideFilterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
