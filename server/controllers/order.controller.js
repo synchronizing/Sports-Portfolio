437c39902a5a94708d29a42fc28c707a8b26a7da
timdoozy <thinds96@ufl.edu>
2019-04-23 17:37:34 -0400

    Update order.controller.js

--- server/controllers/order.controller.js
+++ server/controllers/order.controller.js
@@ -46,7 +46,7 @@ exports.create = function (req, res) {
     
     var mailOptions = {
         from: 'teamsportics@gmail.com',
-        to: 'imnoahcook@gmail.com',
+        to: 'felipefaria@me.com',
         subject: 'New Order',
         text: x  
     };
